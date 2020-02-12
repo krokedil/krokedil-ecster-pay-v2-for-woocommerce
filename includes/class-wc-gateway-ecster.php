3e8421ee4eda7002361e540ca0afb55c47f01666
MichaelBengtsson <michael@krokedil.se>
2020-02-12 09:11:19 +0100

    Fixed text domain and moved where we set the payment method title.

--- includes/class-wc-gateway-ecster.php
+++ includes/class-wc-gateway-ecster.php
@@ -232,6 +232,13 @@ class WC_Gateway_Ecster extends WC_Payment_Gateway {
 		self::log( 'Process payment for order ID ' . $order_id . '. Ecster internal reference ' . $internal_reference . '. Response body - ' . json_encode( $response_body ) );
 
 		if ( $ecster_status ) {
+
+			// Payment method title.
+			$payment_method_title = wc_ecster_get_payment_method_name( $response_body->properties->method );
+			$order->add_order_note( sprintf( __( 'Payment via Ecster Pay %s.', 'krokedil-ecster-pay-for-woocommerce' ), $payment_method_title ) );
+			$order->set_payment_method_title( apply_filters( 'wc_ecster_payment_method_title', sprintf( __( '%s via Ecster Pay', 'krokedil-ecster-pay-for-woocommerce' ), $payment_method_title ), $payment_method_title ) );
+			$order->save();
+
 			// Check Ecster order status
 			switch ( $ecster_status ) {
 				case 'PENDING_SIGNATURE': // Part payment with no contract signed yet
@@ -247,16 +254,8 @@ class WC_Gateway_Ecster extends WC_Payment_Gateway {
 						$order->add_order_note( __( 'Thank you page rendered but purchase in Ecster is not finalized. Ecster status: ' . $ecster_status, 'krokedil-ecster-pay-for-woocommerce' ) );
 					break;
 			}
-
-			// Payment method title.
-			$payment_method_title = wc_ecster_get_payment_method_name( $response_body->properties->method );
-
-			$order->add_order_note( sprintf( __( 'Payment via Ecster Pay %s.', 'krokedil-ecster-pay-for-woocommerce' ), $payment_method_title ) );
-			$order->set_payment_method_title( apply_filters( 'wc_ecster_payment_method_title', sprintf( __( '%s via Ecster Pay', 'krokedil-ecster-pay-for-woocommerce' ), $payment_method_title ), $payment_method_title ) );
-			$order->save();
-
 		} else {
-			// No Ecster order sttus detected
+			// No Ecster order status detected
 			$order->add_order_note( __( 'Thank you page rendered but no Ecster order status was decected.', 'krokedil-ecster-pay-for-woocommerce' ) );
 		}
 
