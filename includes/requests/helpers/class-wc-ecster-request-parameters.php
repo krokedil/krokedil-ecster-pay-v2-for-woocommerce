ece2decceb25b4ecdb4101b29a15b488e7923e2b
MichaelBengtsson <michael@krokedil.se>
2020-02-17 08:59:39 +0100

    Added default delivery country, and better country selector for language.

--- includes/requests/helpers/class-wc-ecster-request-parameters.php
+++ includes/requests/helpers/class-wc-ecster-request-parameters.php
@@ -7,10 +7,11 @@ class WC_Ecster_Request_Parameters {
 
 	public static function get_parameters( $customer_type = 'b2c' ) {
 		return array(
-			'shopTermsUrl' => get_permalink( wc_get_page_id( 'terms' ) ),
-			'purchaseType' => array(
+			'shopTermsUrl'           => get_permalink( wc_get_page_id( 'terms' ) ),
+			'purchaseType'           => array(
 				'type' => $customer_type,
 			),
+			'defaultDeliveryCountry' => WC()->customer->get_billing_country(),
 		);
 	}
 
