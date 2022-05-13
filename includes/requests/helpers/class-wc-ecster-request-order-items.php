b1269b524c412138f9e0f8922d70a4495df60640
Kontrazen <krlovic@gmail.com>
2022-05-13 10:49:41 +0200

    Update with fixed rounding logic

--- includes/requests/helpers/class-wc-ecster-request-order-items.php
+++ includes/requests/helpers/class-wc-ecster-request-order-items.php
@@ -5,144 +5,181 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Gets Order Items.
- * 
+ *
  * @class    WC_Ecster_Get_Order_Items
  * @package  Ecster/Classes/Requests/Helpers
  * @category Class
  * @author   Krokedil <info@krokedil.se>
  */
 class WC_Ecster_Get_Order_Items {
-    /**
-     * Gets items.
-     *
-     * @param int $order_id
-     * @return array
-     */
-    public static function get_items( $order_id ) {
-        $order = wc_get_order( $order_id );
-        $line_number = 0;
-        $items = array();
-        foreach( $order->get_items() as $item ) {
-            $line_number = $line_number + 1;
-            $formated_item = self::get_item( $item, $line_number );
-            array_push( $items, $formated_item );
+	/**
+	 * Gets items.
+	 *
+	 * @param int $order_id
+	 * @return array
+	 */
+	public static function get_items( $order_id ) {
+		$order       = wc_get_order( $order_id );
+		$line_number = 0;
+		$items       = array();
+		foreach ( $order->get_items() as $item ) {
+			$line_number   = $line_number + 1;
+			$formated_item = self::get_item( $item, $line_number );
+			array_push( $items, $formated_item );
 		}
-		
-        foreach( $order->get_shipping_methods() as $shipping_method ) {
-            $line_number = $line_number + 1;
-            $formated_shipping = self::get_shipping( $shipping_method, $line_number );
-            array_push( $items, $formated_shipping );
+
+		foreach ( $order->get_shipping_methods() as $shipping_method ) {
+			$line_number       = $line_number + 1;
+			$formated_shipping = self::get_shipping( $shipping_method, $line_number );
+			array_push( $items, $formated_shipping );
 		}
-		
-        foreach( $order->get_fees() as $fee ) {
-            $line_number = $line_number + 1;
-            $formated_fee = self::get_fee( $fee, $line_number );
-            array_push( $items, $formated_fee );
-        }
-        return $items;
-    }
 
-    /**
-     * Gets single item.
-     *
-     * @param array $item
-     * @return array
-     */
-    private static function get_item( $item ) {
-        $product = $item->get_product();
+		foreach ( $order->get_fees() as $fee ) {
+			$line_number  = $line_number + 1;
+			$formated_fee = self::get_fee( $fee, $line_number );
+			array_push( $items, $formated_fee );
+		}
 
-        if ( $item['variation_id'] ) {
-            $product_id = $item['variation_id'];
-        } else {
-            $product_id = $item['product_id'];
-        }
-        return array(
-            'partNumber'	=>  self::get_sku( $product, $product_id ),
-            'name'          =>  $product->get_name(),
-            'unitAmount'	=>  round( ( $item->get_total() + $item->get_total_tax() ) / $item['qty'] * 100 ),
-            'vatRate'   	=>  self::product_vat_rate( $item ),
-            'quantity'		=>  $item['qty'],
-        );
-    }
+		$rounding_item = self::rounding_item( $items, $order_id );
+		array_push( $items, $rounding_item );
 
-    /**
-     * Gets shipping
-     *
-     * @param string $shipping_method
-     * @param int $line_number
-     * @return array
-     */
-    private static function get_shipping( $shipping_method, $line_number ) {
-        $free_shipping = false;
-        if( 0 === intval( $shipping_method->get_total() ) ) {
-            $free_shipping = true;
-        }
-
-        return array(
-            'partNumber'            =>  'Shipping',
-            'name'                  =>  $shipping_method->get_method_title(),
-            'unitAmount'            =>  ( $free_shipping ) ? 0 : ($shipping_method->get_total() + $shipping_method->get_total_tax() ) * 100,
-            'vatRate'               =>  ( $free_shipping ) ? 0 : ( $shipping_method->get_total_tax() / $shipping_method->get_total() ) * 10000,
-            'quantity'              =>  1,
-        );
-    }
-
-    /**
-     * Gets order Fee.
-     *
-     * @param array $fee
-     * @param int $line_number
-     * @return array
-     */
-    private static function get_fee( $fee, $line_number ) {
-        $fee_vat_code = 0;
-        if( isset( $fee->tax ) && 0 !== $fee->tax ) {
-            $fee_tax_rate = round( $fee->tax / $fee->amount * 100 );
-            if ( in_array( $fee_tax_rate, array( 0, 6, 12, 25 ) ) ) {
-                $fee_vat_code = $fee_tax_rate * 100;
-            }
-        }
-
-        return array(
-			'name'				=>  $fee->get_name(),
-			'description'		=>  $fee->get_id(),
-            'unitAmount'		=>  ($fee->get_total() + $fee->get_total_tax()) * 100,
-            'vatRate'			=>  $fee_vat_code,
-            'quantity'			=>  1,
-        );
-    }
-
-    /**
-     * Calculates tax
-     *
-     * @param array $product
-     * @return int
-     */
-    private static function calculate_tax( $product ) {
-        $price_incl_tax = wc_get_price_including_tax( $product );
-        $price_excl_tax = wc_get_price_excluding_tax( $product );
-        $price_difference = $price_incl_tax - $price_excl_tax;
-        $tax_percent = intval( ( $price_difference / $price_excl_tax ) * 100 );
-
-        return $tax_percent;
-    }
-
-    /**
-     * Gets SKU
-     *
-     * @param array $product
-     * @param int $product_id
-     * @return string
-     */
-    private static function get_sku( $product, $product_id ) {
-        if ( get_post_meta( $product_id, '_sku', true ) !== '' ) {
-            $part_number = $product->get_sku();
-        } else {
-            $part_number = $product->get_id();
-        }
-        return substr( $part_number, 0, 32 );
+		return $items;
 	}
-	
+
+	/**
+	 * Gets single item.
+	 *
+	 * @param array $item
+	 * @return array
+	 */
+	private static function get_item( $item ) {
+		$product = $item->get_product();
+
+		if ( $item['variation_id'] ) {
+			$product_id = $item['variation_id'];
+		} else {
+			$product_id = $item['product_id'];
+		}
+		return array(
+			'partNumber' => self::get_sku( $product, $product_id ),
+			'name'       => $product->get_name(),
+			'unitAmount' => round( ( $item->get_total() + $item->get_total_tax() ) / $item['qty'] * 100 ),
+			'vatRate'    => self::product_vat_rate( $item ),
+			'quantity'   => $item['qty'],
+		);
+	}
+
+	/**
+	 * Rounding adjustment for total amount.
+	 *
+	 * @param array  $items Order lines array.
+	 * @param string $order_id the WooCommerce Order ID.
+	 * @return array
+	 */
+	private static function rounding_item( $items, $order_id ) {
+
+		$formatted_total_amount = 0;
+		foreach ( $items as $key ) {
+			$formatted_total_amount += ( $key['unitAmount'] * $key['quantity'] );
+		}
+
+		$order            = wc_get_order( $order_id );
+		$amount           = ( $order->get_total() ) * 100;
+		$amount_to_adjust = $amount - $formatted_total_amount;
+
+		$ecster_rounding_line = array(
+			'name'       => 'rounding-fee', // Mandatory.
+			'unitAmount' => 0,     // Mandatory.
+			'vatRate'    => 0,       // Mandatory.
+			'quantity'   => 1,                     // Mandatory
+
+		);
+
+		if ( 0 !== $amount_to_adjust ) {
+			$ecster_rounding_line['unitAmount'] = $amount_to_adjust;
+		}
+
+		return $ecster_rounding_line;
+	}
+
+	/**
+	 * Gets shipping
+	 *
+	 * @param string $shipping_method
+	 * @param int    $line_number
+	 * @return array
+	 */
+	private static function get_shipping( $shipping_method, $line_number ) {
+		$free_shipping = false;
+		if ( 0 === intval( $shipping_method->get_total() ) ) {
+			$free_shipping = true;
+		}
+
+		return array(
+			'partNumber' => 'Shipping',
+			'name'       => $shipping_method->get_method_title(),
+			'unitAmount' => ( $free_shipping ) ? 0 : ( $shipping_method->get_total() + $shipping_method->get_total_tax() ) * 100,
+			'vatRate'    => ( $free_shipping ) ? 0 : ( $shipping_method->get_total_tax() / $shipping_method->get_total() ) * 10000,
+			'quantity'   => 1,
+		);
+	}
+
+	/**
+	 * Gets order Fee.
+	 *
+	 * @param array $fee
+	 * @param int   $line_number
+	 * @return array
+	 */
+	private static function get_fee( $fee, $line_number ) {
+		$fee_vat_code = 0;
+		if ( isset( $fee->tax ) && 0 !== $fee->tax ) {
+			$fee_tax_rate = round( $fee->tax / $fee->amount * 100 );
+			if ( in_array( $fee_tax_rate, array( 0, 6, 12, 25 ) ) ) {
+				$fee_vat_code = $fee_tax_rate * 100;
+			}
+		}
+
+		return array(
+			'name'        => $fee->get_name(),
+			'description' => $fee->get_id(),
+			'unitAmount'  => ( $fee->get_total() + $fee->get_total_tax() ) * 100,
+			'vatRate'     => $fee_vat_code,
+			'quantity'    => 1,
+		);
+	}
+
+	/**
+	 * Calculates tax
+	 *
+	 * @param array $product
+	 * @return int
+	 */
+	private static function calculate_tax( $product ) {
+		$price_incl_tax   = wc_get_price_including_tax( $product );
+		$price_excl_tax   = wc_get_price_excluding_tax( $product );
+		$price_difference = $price_incl_tax - $price_excl_tax;
+		$tax_percent      = intval( ( $price_difference / $price_excl_tax ) * 100 );
+
+		return $tax_percent;
+	}
+
+	/**
+	 * Gets SKU
+	 *
+	 * @param array $product
+	 * @param int   $product_id
+	 * @return string
+	 */
+	private static function get_sku( $product, $product_id ) {
+		if ( get_post_meta( $product_id, '_sku', true ) !== '' ) {
+			$part_number = $product->get_sku();
+		} else {
+			$part_number = $product->get_id();
+		}
+		return substr( $part_number, 0, 32 );
+	}
+
 	/**
 	 * @param $cart_item
 	 *
